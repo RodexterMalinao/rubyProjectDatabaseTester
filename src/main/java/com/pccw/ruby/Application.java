78eeb549777aef2f24d473d6d3944d295a5bab5f
Rodexter Malinao <acer_08_06@yahoo.com>
2020-10-28 17:45:03 +0800

    Initial

--- src/main/java/com/pccw/ruby/Application.java
+++ src/main/java/com/pccw/ruby/Application.java
@@ -35,39 +35,6 @@ public class Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-//        System.out.println("DATASOURCE = " + dataSource);
-
-//        System.out.println("Connection = " + dataSource.getConnection());
-
-//        System.out.println("\n1.findAll()...");
-//        for (Customer customer : customerRepository.findAll()) {
-//            System.out.println(customer);
-//        }
-//
-//        System.out.println("\n2.findByEmail(String email)...");
-//        for (Customer customer : customerRepository.findByEmail("222@yahoo.com")) {
-//            System.out.println(customer);
-//        }
-//
-//        System.out.println("\n3.findByDate(Date date)...");
-//        for (Customer customer : customerRepository.findByDate(sdf.parse("2017-02-12"))) {
-//            System.out.println(customer);
-//        }
-//
-//        // For Stream, need @Transactional
-//        System.out.println("\n4.findByEmailReturnStream(@Param(\"email\") String email)...");
-//        try (Stream<Customer> stream = customerRepository.findByEmailReturnStream("333@yahoo.com")) {
-//            stream.forEach(x -> System.out.println(x));
-//        }
-//
-//        //System.out.println("....................");
-//        //SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-//        //Date from = sdf.parse("2017-02-15");
-//        //Date to = sdf.parse("2017-02-17");
-//
-//        //for (Customer customer : customerRepository.findByDateBetween(from, to)) {
-//        //    System.out.println(customer);
-//        //}
 
 		if(dataSource.getConnection()!=null) {
 			System.out.println("Test connection successfully!");	
