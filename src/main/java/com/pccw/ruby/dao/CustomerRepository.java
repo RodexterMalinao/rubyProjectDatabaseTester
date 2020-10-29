523522b39501f81bdb5c3d103c720639922aeb80
Rodexter Malinao <acer_08_06@yahoo.com>
2020-10-29 17:29:03 +0800

    initial

--- src/main/java/com/pccw/ruby/dao/CustomerRepository.java
+++ src/main/java/com/pccw/ruby/dao/CustomerRepository.java
@@ -12,12 +12,12 @@
 //
 //public interface CustomerRepository extends CrudRepository<Customer, Long> {
 //
-//    List<Customer> findByEmail(String email);
-//
-//    @Query("select c from Customer c where c.email = :email")
-//    Stream<Customer> findByEmailReturnStream(@Param("email") String email);
-//
-//    List<Customer> findByDate(Date date);
+////    List<Customer> findByEmail(String email);
+////
+////    @Query("select c from Customer c where c.email = :email")
+////    Stream<Customer> findByEmailReturnStream(@Param("email") String email);
+////
+////    List<Customer> findByDate(Date date);
 //
 //    //@Query("select c from Customer c")
 //    //Stream<Customer> findAllAndStream();
